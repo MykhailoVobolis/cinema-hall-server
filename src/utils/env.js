ed5b930f6de635cd7bf24e3e3be3e5c3d812ba44
MykhailoVobolis <mvobolis@gmail.com>
2024-07-23 12:57:32 +0300

    preparation for deploy

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,6 +2,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+export const ENV_VARS = {
+  GOOGLE_AUTH_CLIENT_ID: 'GOOGLE_AUTH_CLIENT_ID',
+  GOOGLE_AUTH_CLIENT_SECRET: 'GOOGLE_AUTH_CLIENT_SECRET',
+};
+
 export function env(name, defaultValue) {
   const value = process.env[name];
 
--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -2,7 +2,7 @@ import { OAuth2Client } from 'google-auth-library';
 import path from 'node:path';
 import { readFile } from 'fs/promises';
 
-import { env } from './env.js';
+import { env, ENV_VARS } from './env.js';
 import createHttpError from 'http-errors';
 
 const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
@@ -10,8 +10,8 @@ const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
 const oauthConfig = JSON.parse(await readFile(PATH_JSON));
 
 const googleOAuthClient = new OAuth2Client({
-  clientId: env('GOOGLE_AUTH_CLIENT_ID'),
-  clientSecret: env('GOOGLE_AUTH_CLIENT_SECRET'),
+  clientId: env(ENV_VARS.GOOGLE_AUTH_CLIENT_ID),
+  clientSecret: env(ENV_VARS.GOOGLE_AUTH_CLIENT_SECRET),
   redirectUri: oauthConfig.web.redirect_uris[0],
 });
 
