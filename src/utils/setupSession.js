dbf217e480003ffa362d9a3dea7d6b7043172322
MykhailoVobolis <mvobolis@gmail.com>
2024-07-29 21:19:17 +0300

    add client browser detection

--- src/utils/setupSession.js
+++ src/utils/setupSession.js
@@ -2,17 +2,27 @@ import { REFRESH_TOKEN_LIFETIME } from '../constants/index.js';
 
 // Встановлення куків:
 
+// Визначеня браузера клієнта
+const isSafari = (userAgent) => {
+  return /^((?!chrome|android).)*safari/i.test(userAgent);
+};
+
 // Функція встановлює два сесіонні куки для відповіді які доступні тільки через HTTP-запити і не можуть бути доступними через JavaScript на стороні клієнта
-export const setupSession = (res, session) => {
+export const setupSession = (req, res, session) => {
+  const userAgent = req.headers['user-agent'];
+  const isSafariBrowser = isSafari(userAgent);
+
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
-    sameSite: 'None', // Обов'язково для кросс-доменних запитів
+    // sameSite: 'None', // Обов'язково для кросс-доменних запитів
+    sameSite: isSafariBrowser ? 'Strict' : 'None', // Для Safari встановлюємо 'Strict' для інших браузерів 'None'
     secure: process.env.NODE_ENV === 'production', // Встановлюємо true тільки в продакшн
     expires: new Date(Date.now() + REFRESH_TOKEN_LIFETIME),
   });
   res.cookie('sessionId', session._id, {
     httpOnly: true,
-    sameSite: 'None', // Обов'язково для кросс-доменних запитів
+    // sameSite: 'None', // Обов'язково для кросс-доменних запитів
+    sameSite: isSafariBrowser ? 'Strict' : 'None', // Для Safari встановлюємо 'Strict' для інших браузерів 'None'
     secure: process.env.NODE_ENV === 'production', // Встановлюємо true тільки в продакшн
     expires: new Date(Date.now() + REFRESH_TOKEN_LIFETIME),
   });
