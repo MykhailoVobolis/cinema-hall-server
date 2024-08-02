e9ced23943b78abbd006c870b5004a4879bfa8e9
MykhailoVobolis <mvobolis@gmail.com>
2024-08-03 00:31:22 +0300

    change sameSite

--- src/utils/setupSession.js
+++ src/utils/setupSession.js
@@ -30,8 +30,7 @@ export const setupSession = (res, session) => {
   // Определение параметров sameSite и secure в зависимости от браузера
   const cookieOptions = {
     httpOnly: true,
-    // sameSite: isSafari ? 'Lax' : 'None', // Для Safari используем 'Lax', для остальных - 'None'
-    sameSite: isSafari ? 'Strict' : 'None',
+    sameSite: isSafari ? 'Lax' : 'None', // Для Safari используем 'Lax', для остальных - 'None'
     secure: process.env.NODE_ENV === 'production', // Устанавливаем true только в продакшн
     expires: new Date(Date.now() + REFRESH_TOKEN_LIFETIME),
   };
