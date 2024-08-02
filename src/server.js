858c46cc119c7db11994daaa42222bffc0337032
MykhailoVobolis <mvobolis@gmail.com>
2024-08-03 00:39:52 +0300

    delete setting cors title

--- src/server.js
+++ src/server.js
@@ -33,18 +33,6 @@ export const startServer = () => {
 
   app.use(cors(corsOptions));
 
-  // Налаштування заголовкшв CORS
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    );
-    next();
-  });
-
   app.use(cookieParser());
 
   app.use(router);
