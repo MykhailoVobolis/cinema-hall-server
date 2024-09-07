23e8e440f2756a2ea0d0a9fc575ef7247837916b
MykhailoVobolis <mvobolis@gmail.com>
2024-09-08 00:39:37 +0300

    change sameSite value

--- src/utils/setupSession.js
+++ src/utils/setupSession.js
@@ -20,10 +20,20 @@ import { REFRESH_TOKEN_LIFETIME } from '../constants/index.js';
 
 // Рефакторінг
 export const setupSession = (res, session) => {
+  // Перевірка чи браузер клієнта Safari
+  const userAgent = res.req.headers['user-agent'];
+
+  const isSafari =
+    userAgent.includes('Safari') &&
+    !userAgent.includes('Chrome') &&
+    !userAgent.includes('Chromium');
+
+  // Определение параметров sameSite и secure в зависимости от браузера
   const cookieOptions = {
     httpOnly: true,
-    sameSite: 'None',
-    secure: process.env.NODE_ENV === 'production', // Встановлюємо true тільки в продакшн
+    sameSite: isSafari ? 'Lax' : 'None', // Для Safari используем 'Lax', для остальных - 'None'
+    // sameSite: 'None',
+    secure: process.env.NODE_ENV === 'production', // Устанавливаем true только в продакшн.
     expires: new Date(Date.now() + REFRESH_TOKEN_LIFETIME),
   };
 
